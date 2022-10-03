448830a4ba2c94ad40ec7f5fed931c4626139a65
Eftal Gezer <eftalgezer@gmail.com>
2022-10-03 23:09:04 +0300

    Add files via upload

--- tests/tests.py
+++ tests/tests.py
@@ -10,5 +10,5 @@ MPATH = mfile.replace("/fstring_to_format/__init__.py", "")
 
 def test_main():
     """Tests for __main__.py"""
-    os.chdir(f"{MPATH}{os.sep}tests{os.sep}assets")
+    os.chdir("{0}{1}tests{1}assets".format(MPATH, os.sep))
     assert "All files are converted" in main_tester("fstring_to_format file1.py.test")
