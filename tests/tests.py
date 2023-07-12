9ba30534b3d1eceec40ffe0f6d66a988c40c5ea8
Eftal Gezer <eftalgezer@gmail.com>
2023-07-12 11:46:25 +0300

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -3,7 +3,7 @@ Unit tests for the fstring_to_format package.
 """
 import os
 from fstring_to_format import __file__ as mfile
-from .testers import main_tester, unique_tester
+from .testers import main_tester
 
 MPATH = os.path.dirname(mfile)
 
@@ -12,6 +12,3 @@ def test_main():
     os.chdir(os.path.join(MPATH, "tests", "assets"))
     assert "All files are converted" in main_tester("fstring_to_format file*.py.test")
     assert "No files are found" in main_tester("fstring_to_format Foo.py.test")
-
-def test_unique():
-    assert unique_tester(["{mpath}", "{mpath}", "{os.sep}", "{os.sep}"]) == ["{mpath}", "{os.sep}"]
