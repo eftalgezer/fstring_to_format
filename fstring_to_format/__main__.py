4c3eb94d3a2e1627aec87d2e15810fc65ee499d7
Eftal Gezer <eftalgezer@gmail.com>
2023-07-12 04:43:54 +0300

    Optimisation.

--- fstring_to_format/__main__.py
+++ fstring_to_format/__main__.py
@@ -1,17 +1,12 @@
-"""
-fstring_to_format terminal client
-"""
-
-from __future__ import absolute_import
 import sys
-
 from .core import formatify
 
-
-def main(args):
+def main():
     """Main function"""
-    formatify(args[1])
-
+    if len(sys.argv) < 2:
+        print("Path argument is missing")
+        return
+    formatify(sys.argv[1])
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
