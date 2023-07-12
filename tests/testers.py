63932876c4e9ca8114b12c62511aca6d88eb11b5
Eftal Gezer <eftalgezer@gmail.com>
2023-07-12 04:45:14 +0300

    Optimisation.

--- tests/testers.py
+++ tests/testers.py
@@ -1,15 +1,9 @@
-"""
-Unit testers for the fstring_to_format package.
-"""
-from __future__ import absolute_import
 import io
-import sys
 import os
 from fstring_to_format import __file__ as mfile
 from fstring_to_format.core import unique
 
-MPATH = mfile.replace("{0}fstring_to_format{0}__init__.py".format(os.sep), "").replace("fstring_to_formatc", "fstring_to_format")
-
+MPATH = os.path.dirname(mfile)
 
 def main_tester(command):
     """Tester for __main__.py"""
